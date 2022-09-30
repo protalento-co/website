b3f3b5685628b85c34d138ddc4ee0c8bfc186ce6
Nicolasg <104913871+NicolasGarciaR@users.noreply.github.com>
2022-09-30 11:28:37 -0500

    Feature/content (#114)
    * Fixing content in landings
    
    * Asodamas partners and form
    
    * Adding full content to landings
    
    * Details in all pages
    
    * remove asodamas and fixing content
    
    * Fixing features
    
    * tyc
    
    * Reformat images
    
    * change img for icons home
    
    * More img to icons home
    
    * English icons
    
    * Footer imgs to icons
    
    * Last icons in webpage
    
    * Passing code for W3C Validator and fix errors and warnings
    
    * New content about financiation
    
    * Fix double button
    
    * Duplicate index
    
    * Lighthouse fixes
    
    * Adding clarity to the project
    
    * Removing a couple of buttons
    
    * fixing styles & text in Asodamas
    
    * adding prices to Asodamas
    
    * Fixing texts & img Asodamas
    
    * Adding new img of Primera
    
    * Adding ada img
    
    * fixing asodamas & bvc imgs
    
    * Fix style in cover
    
    * fix style in cover
    
    * Adding didi landing and fix DWFS price
    
    * Fixing full stack python link & adding asodamas
    
    * Adding DiDi landing
    
    * fix hidden
    
    * a
    
    * Adding didi features & dates

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,6 +1,12 @@
 let toggle = document.getElementById('toggle');
 let nav = document.querySelector('.nav');
+let didiNav = document.querySelector('.nav__didi');
+let whiteLogo = document.getElementById('white__logo');
+let blueLogo = document.getElementById('blue__logo');
+
 
 toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
+  blueLogo.classList.toggle('d-none');
+  whiteLogo.classList.toggle('d-none');
 });
