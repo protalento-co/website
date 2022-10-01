d958b4a206969b0b8fb3f0dcea4cbec1ba79b30c
Nicolasg <104913871+NicolasGarciaR@users.noreply.github.com>
2022-10-01 17:12:47 -0500

    Feature/content (#128)
    * Adding didi features & dates
    
    * Addin new nav to landings
    
    * fix margin top in postulacion landings
    
    * fix new nav error
    
    * Edit hero sizes
    
    * Adding info-prices to Landing
    
    * Fix prices and classes information
    
    * Addin 5 cards in "how to pay" asodamas

--- js/comoPagar.js
+++ js/comoPagar.js
@@ -12,7 +12,7 @@ function toggleText() {
     } else {
         hideText_btn.innerHTML = 'Ver Mas'
     }
-}
+};
 
 
 let hideText_btn2 = document.getElementById('hideText_btn2');
@@ -27,5 +27,35 @@ function toggleText2() {
         hideText_btn2.innerHTML = 'Ver Menos'
     } else {
         hideText_btn2.innerHTML = 'Ver Mas'
+    }
+};
+
+let hideText_btn3 = document.getElementById('hideText_btn3');
+let hideText3 = document.getElementById('hideText3');
+
+hideText_btn3.addEventListener('click', toggleText3);
+
+function toggleText3() {
+    hideText3.classList.toggle('show');
+
+    if(hideText3.classList.contains('show')) {
+        hideText_btn3.innerHTML = 'Ver Menos'
+    } else {
+        hideText_btn3.innerHTML = 'Ver Mas'
+    }
+}
+
+let hideText_btn4 = document.getElementById('hideText_btn4');
+let hideText4 = document.getElementById('hideText4');
+
+hideText_btn4.addEventListener('click', toggleText4);
+
+function toggleText4() {
+    hideText4.classList.toggle('show');
+
+    if(hideText4.classList.contains('show')) {
+        hideText_btn4.innerHTML = 'Ver Menos'
+    } else {
+        hideText_btn4.innerHTML = 'Ver Mas'
     }
 }
