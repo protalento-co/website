17056fecf1d0497ce553009bdd3fec6ae08324b4
NicolasGarciaR <nicolasg@protalento.org>
2022-10-06 12:30:38 -0500

    Adding footer & nav to all

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,4 +1,5 @@
 let toggle = document.getElementById('toggle');
+let toggle1 = document.getElementById('toggle1');
 let nav = document.querySelector('.nav');
 let didiNav = document.querySelector('.nav__didi');
 let whiteLogo = document.getElementById('white__logo');
@@ -9,4 +10,11 @@ toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
   blueLogo.classList.toggle('d-none');
   whiteLogo.classList.toggle('d-none');
+});
+
+toggle1.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  blueLogo.classList.toggle('d-none');
+  whiteLogo.classList.toggle('d-none');
+
 });
