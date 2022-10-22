b63909f2889f05479adaffb14baeb7b2f9b56ade
SrPio <andresfer1799@gmail.com>
2022-10-22 17:16:41 -0500

    fix(postulacion-co y mx): accordeons Se solucionó el funcionamiento de los accordeons con codigo embed

--- js/acordeon.js
+++ js/acordeon.js
@@ -6,7 +6,7 @@ const accordeonItemFaq = document.querySelectorAll('.item-faq');
 
 for (let i = 0; i < accordeonTitle.length; i++) {
     const itemFaq = accordeonItemFaq[i];
-    
+
     itemFaq.addEventListener('click', () => {
         const index = i;
 
@@ -15,12 +15,12 @@ for (let i = 0; i < accordeonTitle.length; i++) {
         accordeonArrow[index].classList.toggle('arrow-rotate');
 
         for (let f = 0; f < accordeonTitle.length; f++) {
-            if(f !== i) {
+            if (f !== i) {
                 accordeonTitle[f].classList.remove('active');
                 accordeonContent[f].classList.remove('active');
                 accordeonItemFaq[f].classList.remove('active');
                 accordeonArrow[f].classList.remove('arrow-rotate');
-            }               
+            }
         }
 
     })
@@ -33,7 +33,7 @@ const accordeonItemNav = document.querySelectorAll('.item-nav');
 
 for (let i = 0; i < accordeonTitle2.length; i++) {
     const itemNav = accordeonItemNav[i];
-    
+
     itemNav.addEventListener('click', () => {
         const index = i;
 
@@ -44,13 +44,13 @@ for (let i = 0; i < accordeonTitle2.length; i++) {
 
 
         for (let f = 0; f < accordeonTitle2.length; f++) {
-            if(f !== i) {
+            if (f !== i) {
                 accordeonTitle2[f].classList.remove('active');
                 accordeonContent2[f].classList.remove('active');
                 accordeonContent2[f].classList.add('d-none');
                 accordeonItemNav[f].classList.remove('active');
                 accordeonArrow2[f].classList.remove('arrow-rotate');
-            }               
+            }
         }
 
     })
@@ -63,7 +63,7 @@ const heroButton3 = document.querySelectorAll('.hero__button')
 
 for (let i = 0; i < accordeonTitle3.length; i++) {
     const title3 = accordeonTitle3[i];
-    
+
     title3.addEventListener('click', () => {
         const index = i;
 
@@ -74,12 +74,12 @@ for (let i = 0; i < accordeonTitle3.length; i++) {
 
 
         for (let f = 0; f < accordeonTitle3.length; f++) {
-            if(f !== i) {
+            if (f !== i) {
                 accordeonTitle3[f].classList.remove('active');
                 accordeonContent3[f].classList.remove('active');
                 accordeonArrow3[f].classList.remove('arrow-rotate3');
                 heroButton3[f].classList.remove('btn-didi-hero');
-            }               
+            }
         }
 
     })
@@ -92,7 +92,7 @@ const accordeonItem = document.querySelectorAll('.item');
 
 for (let i = 0; i < accordeonTitle4.length; i++) {
     const item = accordeonItem[i];
-    
+
     item.addEventListener('click', () => {
         const index = i;
 
@@ -102,13 +102,13 @@ for (let i = 0; i < accordeonTitle4.length; i++) {
         accordeonArrow4[index].classList.toggle('arrow-rotate');
 
         for (let f = 0; f < accordeonTitle4.length; f++) {
-            if(f !== i) {
+            if (f !== i) {
                 accordeonTitle4[f].classList.remove('active');
                 accordeonContent4[f].classList.remove('active');
                 accordeonContent4[f].classList.add('d-none');
                 accordeonItem[f].classList.remove('active');
                 accordeonArrow4[f].classList.remove('arrow-rotate');
-            }               
+            }
         }
 
     })
@@ -123,7 +123,7 @@ const accordeonItemCards = document.querySelectorAll('.item-cards');
 
 for (let i = 0; i < accordeonTitle5.length; i++) {
     const itemCards = accordeonItemCards[i];
-    
+
     itemCards.addEventListener('click', () => {
         const index = i;
 
@@ -133,14 +133,45 @@ for (let i = 0; i < accordeonTitle5.length; i++) {
         accordeonArrow5[index].classList.toggle('arrow-rotate5');
 
         for (let f = 0; f < accordeonTitle5.length; f++) {
-            if(f !== i) {
+            if (f !== i) {
                 accordeonTitle5[f].classList.remove('active');
                 accordeonContent5[f].classList.remove('active');
                 accordeonContent5[f].classList.add('d-none');
                 accordeonItemCards[f].classList.remove('active');
                 accordeonArrow5[f].classList.remove('arrow-rotate5');
-            }               
+            }
         }
 
     })
 }
+
+
+/* para forms embed */
+const accordeonTitleForms = document.querySelectorAll('.accordeon-title--forms');
+const accordeonContentForms = document.querySelectorAll('.accordeon-content--forms');
+const accordeonArrowForms = document.querySelectorAll('.arrow');
+const accordeonItemForms = document.querySelectorAll('.item--forms');
+
+
+for (let i = 0; i < accordeonTitleForms.length; i++) {
+    const itemForms = accordeonItemForms[i];
+
+    itemForms.addEventListener('click', () => {
+        const index = i;
+
+        accordeonTitleForms[index].classList.toggle('active');
+        accordeonContentForms[index].classList.toggle('active');
+        accordeonArrowForms[index].classList.toggle('arrow');
+
+        for (let f = 0; f < accordeonTitleForms.length; f++) {
+            if (f !== i) {
+                accordeonTitleForms[f].classList.remove('active');
+                accordeonContentForms[f].classList.remove('active');
+                accordeonItemForms[f].classList.remove('active');
+                accordeonArrowForms[f].classList.remove('arrow');
+            }
+        }
+
+    })
+}
+/* END para forms embed */
--- js/acordeonForms.js
+++ js/acordeonForms.js
@@ -1,8 +1,7 @@
-const accordeonTitle = document.querySelectorAll('.accordeon-title');
-const accordeonContent = document.querySelectorAll('.accordeon-content');
-const accordeonArrow = document.querySelectorAll('.arrow');
-const accordeonItemFaq = document.querySelectorAll('.item-faq');
-const postulationForm = document.querySelectorAll('.postulation_form');
+const accordeonTitle = document.querySelectorAll('.accordeon-title--forms');
+const accordeonContent = document.querySelectorAll('.accordeon-content--forms');
+const accordeonArrow = document.querySelectorAll('.arrow--forms');
+const accordeonItemFaq = document.querySelectorAll('.item--forms');
 
 
 for (let i = 0; i < accordeonTitle.length; i++) {
@@ -12,41 +11,15 @@ for (let i = 0; i < accordeonTitle.length; i++) {
     const index = i;
 
     accordeonTitle[index].classList.toggle('active');
-    accordeonContent[index].style.display = "inline";
-    postulationForm[index].style.display = "inline";
     accordeonContent[index].classList.toggle('active');
-    accordeonArrow[index].classList.toggle('arrow-rotate');
-
-    const subject = document.querySelector('#subject');
-
-
-    var insertedContent = document.querySelector(".insertedContent");
-    if (insertedContent) {
-      insertedContent.parentNode.removeChild(insertedContent);
-    }
-    /* el.insertAdjacentHTML('afterend', "<span class ='insertedContent'>foo bar</span>"); */
-    subject.insertAdjacentHTML('afterend', `<div class="external-form">
-        <div
-          class="calendly-inline-widget"
-          data-url="https://calendly.com/protalento/entrevista-candidato?hide_gdpr_banner=1&text_color=20212f&primary_color=f1af4d"
-          style="height: 630px; width: 95%"
-        ></div>
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        ></script>
-      </div>`);
+    accordeonArrow[index].classList.toggle('arrow-rotate--forms');
 
     for (let f = 0; f < accordeonTitle.length; f++) {
       if (f !== i) {
-        /* document.location.reload(); */
-        accordeonContent[f].style.display = "none";
-        /* postulationForm[f].style.display = "none"; */
         accordeonTitle[f].classList.remove('active');
         accordeonContent[f].classList.remove('active');
         accordeonItemFaq[f].classList.remove('active');
-        accordeonArrow[f].classList.remove('arrow-rotate');
+        accordeonArrow[f].classList.remove('arrow-rotate--forms');
       }
     }
 
