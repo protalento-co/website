f09e69d437f512e185f5a0f68ff20922bf9f839f
SrPio <andresfer1799@gmail.com>
2022-11-10 15:47:03 -0500

    feat(promigas): sliding menu promigas--details Se creó la barra del sliding menú en promigas--details.

---
+++ js/programaAsodamas2.js
@@ -0,0 +1,48 @@
+const boxes = document.querySelectorAll('.details__icons');
+
+
+var marker = document.querySelector("#marker");
+
+function indicator(e) {
+  marker.style.left = e.offsetLeft + "px";
+  marker.style.width = e.offsetWidth + "px";
+
+  /* console.log(marker); */
+}
+
+boxes.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    indicator(e.target);
+  });
+});
+
+
+boxes.forEach((item, idx) => {
+
+  item.id = `boxes-${idx}`
+
+  item.addEventListener('click', () => {
+
+    // change box
+    boxes.forEach((item, id) => {
+      /* item.classList.remove('bg-blueLight'); */
+      change(id + 1, 'none');
+    });
+
+    // 
+    /* if (item.id === `boxes-${idx}`) item.classList.add('bg-blueLight'); */
+
+    change(idx + 1, '');
+  });
+
+  change(idx + 2, 'none');
+});
+
+// re-construccion
+function change(number, display) {
+  const grupo = document.querySelectorAll(`.group-${number}`);
+
+  grupo.forEach((item) => {
+    item.style.display = display;
+  });
+}
